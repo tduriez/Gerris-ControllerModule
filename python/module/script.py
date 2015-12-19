d7a7f9e58b6b8cc5ac83f30e2d81ae1e84cb54fe
pablisho <rodriguez.pabloariel@gmail.com>
2015-12-19 15:19:50 -0300

    gnuplot

--- python/module/script.py
+++ python/module/script.py
@@ -1,8 +1,23 @@
+lastFF = 0.6
+lastFF2 = 0.1
+lastDrag = 0.1
 
 def ffForce(forcesList,locList):
+	global lastFF
+	global lastFF2
+	global lastDrag
 	if len(forcesList) > 0:
 		force = forcesList[0]
-		return force.data.pf[0]
+#		print str(lastFF)
+#		print str(lastDrag)
+		drag = force.data.pf[0]*force.data.pf[0] + force.data.pf[1]*force.data.pf[1]
+		if drag != lastDrag:
+			ff = lastFF - 0.01 * (drag - lastDrag) / (lastFF - lastFF2)
+			lastFF2 = lastFF
+			lastFF = ff
+			lastDrag = drag 
+#		print str(ff)
+		return lastFF
 	return 1
 
 def ffpos(forcesList,locList):
